c33fb30cb032de66a3ff291513984889f7c2259d
Frederik Batuna <gobi301@gmail.com>
2017-03-03 03:29:09 -0800

    add: postpublish hook

---
+++ test/sync/string.js
@@ -0,0 +1,31 @@
+// @flow
+/* eslint-env mocha */
+
+import { expect } from 'chai';
+
+import { string, ValidationError } from '../../src';
+
+describe('string', () => {
+  describe('#parse', () => {
+    it('returns on expected type', () => {
+      expect(string.validate('hello')).to.equal('hello');
+    });
+    it('throws on wrong type', () => {
+      const errorShape = got => ({ expected: { name: string.name }, got });
+      try { string.parse(); } catch (e) { expect(e).to.be.instanceof(ValidationError).deep.equal(errorShape(void 0)); }
+      const date = new Date;
+      try { string.parse(date); } catch (e) { expect(e).to.be.instanceof(ValidationError).deep.equal(errorShape(date)); }
+    });
+  });
+  describe('#validate', () => {
+    it('returns on expected type', () => {
+      expect(string.validate('hello')).to.equal('hello');
+    });
+    it('throws on wrong type', () => {
+      const errorShape = got => ({ expected: { name: string.name }, got });
+      try { string.validate(); } catch (e) { expect(e).to.be.instanceof(ValidationError).deep.equal(errorShape(void 0)); }
+      const date = new Date;
+      try { string.validate(date); } catch (e) { expect(e).to.be.instanceof(ValidationError).deep.equal(errorShape(date)); }
+    });
+  });
+});
